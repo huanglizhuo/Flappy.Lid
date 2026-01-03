73de776f3175c0aab0836037834393dc956b3578
huanglizhuo <lizhuo.huang@outlook.com>
2026-01-03 16:21:57 +0800

    Initial Commit

---
+++ Flappy.Lid/Flappy_LidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flappy_LidApp.swift
+//  Flappy.Lid
+//
+//  Created by lizhuo on 2026/01/03.
+//
+
+import SwiftUI
+
+@main
+struct Flappy_LidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
