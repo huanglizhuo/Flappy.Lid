212467eb00aa69e6bc0cd7c5e2b8da144c898202
huanglizhuo <lizhuo.huang@outlook.com>
2026-01-08 22:30:30 +0900

    fix key sim minimize

--- Flappy.Lid/KeySimulatorView.swift
+++ Flappy.Lid/KeySimulatorView.swift
@@ -39,15 +39,6 @@ struct KeySimulatorView: View {
                             .buttonStyle(.plain)
                             
                             Spacer()
-                            
-                            // Settings Button (Enabled)
-//                            Button(action: { showSettings = true }) {
-//                                Image(systemName: "gearshape.fill")
-//                                    .font(.system(size: minDim * 0.06))
-//                                    .foregroundColor(.white)
-//                                    .shadow(radius: 5)
-//                            }
-//                            .buttonStyle(.plain)
                         }
                     }
                     .padding(.horizontal, 20)
